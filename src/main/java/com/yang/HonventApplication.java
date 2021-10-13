55b1c145fdd2a175928057855e00ec7c89b1b7ed
yang09ch <1436933542@qq.com>
2021-10-13 20:19:43 +0800

    测试

--- src/main/java/com/yang/HonventApplication.java
+++ src/main/java/com/yang/HonventApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HonventApplication {
-
+//刘瑞祥
     public static void main(String[] args) {
         SpringApplication.run(HonventApplication.class, args);
     }
